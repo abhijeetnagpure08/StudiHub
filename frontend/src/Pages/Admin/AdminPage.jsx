271d564d837c29b9089c86a633276d90f8ccb44b
abhijeetnagpure08 <abhijeetnagpureofficial@gmail.com>
2023-11-23 23:42:37 +0530

    changes done

--- frontend/src/Pages/Admin/AdminPage.jsx
+++ frontend/src/Pages/Admin/AdminPage.jsx
@@ -1,20 +1,15 @@
-import { Flex } from '@chakra-ui/react'
-import React from 'react'
-import { Sidebar } from '../../Components/AdminComponents/Sidebar'
-import { AdminNavbar } from '../../Components/AdminComponents/AdminNavbar'
-
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { Sidebar } from "../../Components/AdminComponents/Sidebar";
+import { AdminNavbar } from "../../Components/AdminComponents/AdminNavbar";
 
 export const AdminHomepage = () => {
   return (
     <>
-     <Flex>
-     <Sidebar/>
-     <AdminNavbar/>
-     </Flex>
-   
-    
-    
+      <Flex>
+        <Sidebar />
+        <AdminNavbar />
+      </Flex>
     </>
-  )
-}
-
+  );
+};
